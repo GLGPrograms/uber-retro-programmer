e3547f0c468dea0eacbb589e0590bb34f100a88a
giomba <giomba@glgprograms.it>
2021-11-09 14:36:17 +0100

    getopts/parse args from cli

--- swr/src/main.rs
+++ swr/src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg, Parser};
 use log;
 use serialport::available_ports;
 use simple_logger::SimpleLogger;
@@ -9,6 +10,28 @@ fn main() {
     SimpleLogger::new().init().unwrap();
     log::info!("serprog");
 
+    let opts = App::new("serprog")
+        .version("1.0")
+        .author("giomba <giomba@glgprograms.it>")
+        .about("serial programmer ")
+        .arg(
+            Arg::new("device")
+                .short('d')
+                .long("device")
+                .about("serial port file")
+                .default_value("/dev/ttyS0")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("baudrate")
+                .short('b')
+                .long("baudrate")
+                .about("serial port baud rate")
+                .default_value("9600")
+                .takes_value(true),
+        )
+        .get_matches();
+
     let serial_ports = match available_ports() {
         Err(e) => {
             log::error!("cannot enumerate serial ports on this system");
@@ -19,7 +42,16 @@ fn main() {
 
     println!("{:#?}", serial_ports);
 
-    let builder = serialport::new("/dev/ttyUSB1", 9600);
+    let builder = serialport::new(
+        opts.value_of("device").unwrap(),
+        match opts.value_of("baudrate").unwrap().parse() {
+            Ok(r) => r,
+            Err(e) => {
+                log::error!("baudrate: {}", e);
+                exit(exitcode::USAGE)
+            }
+        },
+    );
     let mut port = match builder.open() {
         Err(e) => {
             log::error!("cannot open serial port: {}", e);
