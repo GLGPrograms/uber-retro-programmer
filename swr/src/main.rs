6cd1b2d063fafe161aebaa0ce7bfea71f92f9add
giomba <giomba@glgprograms.it>
2021-11-14 13:46:03 +0100

    write data at address

--- swr/src/main.rs
+++ swr/src/main.rs
@@ -6,7 +6,7 @@ use log::{self, info};
 use serialport::{available_ports, SerialPort};
 use simple_logger::SimpleLogger;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::os::unix::prelude::FileExt;
 use std::{process::exit, time::Duration};
 
@@ -14,9 +14,11 @@ const BUFFER_LEN: usize = 1024;
 
 const S_ACK: u8 = 0x06;
 const S_CMD_Q_R_NBYTES: u8 = 0x0a;
+const S_CMD_O_WRITEN: u8 = 0x0d;
 const S_NAK: u8 = 0x15;
 const S_CMD_NOP: u8 = 0x00;
 const S_CMD_Q_PGMNAME: u8 = 0x03;
+const S_CMD_O_EXEC: u8 = 0x0f;
 
 struct Programmer {
     port: Box<dyn SerialPort>,
@@ -137,7 +139,20 @@ async fn main() -> anyhow::Result<()> {
     }
 
     // if operation is write
-    // TODO
+    if let Some(filename) = opts.value_of("write") {
+        // parse needed arguments
+        let base = opts.value_of("addr").unwrap().parse()?;
+        let size: usize = opts.value_of("size").unwrap().parse()?;
+        println!("writing {} bytes from {}", size, filename);
+
+        // write file
+        let mut data: Vec<u8> = Vec::new();
+        data.resize(size, 0);
+        let mut file = File::open(filename).with_context(|| "cannot open input file")?;
+        file.read_exact(&mut data)?;
+
+        programmer.write_bytes(base, &data).await?;
+    }
 
     Ok(())
 }
@@ -196,6 +211,50 @@ impl Programmer {
         Ok(())
     }
 
+    async fn write_bytes(&mut self, base: usize, data: &[u8]) -> anyhow::Result<()> {
+        let mut done = 0;
+        let mut remaining = data.len();
+        const CHUNK_SIZE: usize = 64;
+
+        self.pb.reset();
+        while done < data.len() {
+            let current_chunk_size = if remaining > CHUNK_SIZE {
+                CHUNK_SIZE
+            } else {
+                remaining
+            };
+            self.write_bytes_chunk(base + done, &data[done..done + current_chunk_size])
+                .await?;
+
+            let percent = (done as f64 / data.len() as f64 * 100.0) as u64;
+            self.pb.set_position(percent);
+            remaining -= current_chunk_size;
+            done += current_chunk_size;
+        }
+
+        Ok(())
+    }
+
+    async fn write_bytes_chunk(&mut self, base: usize, data: &[u8]) -> anyhow::Result<()> {
+        let mut obuffer = Vec::new();
+        obuffer.resize(7, 0);
+        obuffer[0] = S_CMD_O_WRITEN;
+        obuffer[1] = (data.len() & 0xff) as u8;
+        obuffer[2] = (data.len() >> 8 & 0xff) as u8;
+        obuffer[3] = (data.len() >> 16 & 0xff) as u8;
+        obuffer[4] = (base & 0xff) as u8;
+        obuffer[5] = (base >> 8 & 0xff) as u8;
+        obuffer[6] = (base >> 16 & 0xff) as u8;
+
+        let _written = self.port.write(&obuffer)?;
+        let _written = self.port.write(data)?;
+
+        let obuffer = vec![S_CMD_O_EXEC];
+        let _written = self.port.write(&obuffer)?;
+
+        Ok(())
+    }
+
     async fn recv_with_ack(&mut self, data: &mut [u8]) -> anyhow::Result<()> {
         let mut ack_buffer = [0; 1];
         self.port
@@ -213,14 +272,3 @@ impl Programmer {
         Ok(())
     }
 }
-
-/*
-async fn read_bytes(base: usize, length: usize) -> Vec<u8> {
-    assert!(base < (1 << 24));
-    assert!(length < (1 << 24));
-
-    let mut ibuffer
-
-    let mut ibuffer : [u8; ]
-}
-*/
